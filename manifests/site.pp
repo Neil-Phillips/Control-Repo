a1faafeef629ee06a865d465b6535935c527261b
Neil Phillips <skylineinstallation.np@googlemail.com>
2022-10-20 12:02:18 +0100

    KMS xfce panel

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,15 @@
-node 'dev-test-till.hq.toolstation.com' {
-  $location = 'BS'
+node 'puppetdevtill1.hq.toolstation.com' {
+  $location = 'zz'
   include stdlib
   include epos
+  include google_chrome
+}
+
+node 'tos-devtill1.zz.toolstation.shop' {
+  $location = 'zz'
+  include stdlib
+  include epos
+  include google_chrome
 }
 
 node default {
